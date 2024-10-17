1af3b4e31ed16bb8bf019f2e6fe4ad7a32746955
Nicholas <115585646+nickklos10@users.noreply.github.com>
2024-10-17 14:26:01 -0400

    Add sql files

---
+++ clientPermissionsScriptProject3.sql
@@ -0,0 +1,6 @@
+grant select on bikedb.* to client1;
+grant select, update on bikedb.* to client2;
+grant select on project3.* to client1;
+grant select, update on project3.* to client2;
+grant select, insert, update on operationslog.* to project3app;
+grant select on operationslog.* to theaccountant;
